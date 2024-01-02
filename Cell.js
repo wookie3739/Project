cc798a1f0de229d0d4ba5d4406b172eef4a73acc
wookie3739 <dnlsemtmf@gmail.com>
2024-01-02 17:58:36 +0900

    project 1st

---
+++ Cell.js
@@ -0,0 +1,26 @@
+class Cell{
+    constructor(container, x, y, width, height, text, bg){
+        this.container = container;
+        this.div = document.createElement('div');
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.text = text;
+        this.bg = bg;
+
+
+        this.div.style.position = "absolute";
+        this.div.style.left = this.x + "px";
+        this.div.style.top = this.y + "px";
+        this.div.style.width = this.width + "px";
+        this.div.style.height = this.height + "px";
+        this.div.style.background = this.bg;
+        this.div.innerText = this.text;
+        this.div.style.border = "1px solid black";
+        this.div.style.boxSizing = "border-box";
+
+        this.container.appendChild(this.div);
+    }
+    
+}
---
+++ Keyboard.js
@@ -0,0 +1,6 @@
+class Keyboard extends Cell{
+    constructor(container, x, y, width, height, text, bg){
+        super(container, x, y, width, height, text, bg);
+    }
+    
+}
